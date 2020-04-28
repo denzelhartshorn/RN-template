034c9b546ee4399df4cee5a5c3745553bbb16d15
Denzel Hartshorn <denzelhartshorn@gmail.com>
2020-04-27 21:21:54 -0600

    next step

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Stylesheet, Text, View } from "react-native";
+
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title="This is the Header Title" />
-      <StartGameScreen />
     </View>
   );
 }
